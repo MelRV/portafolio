78d2dd90751bfcbf06345770612b9006c1e143a4
Melissa Ramirez <melissaramirez@Melissas-MacBook-Air-2.local>
2024-07-04 18:36:28 -0600

    first page

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menuLinks");
+    const icon = document.querySelector(".burgerIcon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
